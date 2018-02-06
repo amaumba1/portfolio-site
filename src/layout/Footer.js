428c4dd4c561322f34c80733906da78c34dd7d7c
Ally Maumba <amaumba1@gmail.com>
2018-02-05 23:16:43 -0600

    adding footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,18 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            Footer
+            <ul className="nav">
+                <li className="nav__item"><a href="#" className="nav__link">Find your dream home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+            
+            </ul>
+            <p className="copyright">
+                &copy; Copyright 2018 by amaumba1
+            </p>
         </footer>
     );
 }
