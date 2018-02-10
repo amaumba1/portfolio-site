03eed9d7f5b617aa12f5195f879b4d1297db9a05
Ally Maumba <amaumba1@gmail.com>
2018-02-10 16:30:06 -0600

    add  media query

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const ContactPage = () => (
-    <div>
-        <h1>Contact Me</h1>
-        <p>You can reach me at test@gmail.com</p>
+    <div className="challenge">
+        <div className="header1">Header</div>
+        <div className="small-box-1">Small box 1</div>
+        <div className="small-box-2">Small box 2</div>
+        <div className="small-box-3">Small box 3</div>
+        <div className="main-content">Main content</div>
+        <div className="sidebar1">Sidebar</div>
+        <div className="footer1">Footer</div>
     </div>
 )
 
